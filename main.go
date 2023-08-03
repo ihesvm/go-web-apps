f51f64b5d8eb4506e4c902d03e8c588531b193ac
ihesvm <hesamfm@gmail.com>
2023-08-03 18:29:47 +0330

    initial commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func main() {
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed connect to database!!!")
+	}
+
+	db.AutoMigrate(&Book{})
+
+	handler := newHandler(db)
+
+	r := gin.New()
+
+	r.GET("/books", handler.listBooksHandler)
+	r.POST("/books", handler.createBookHandler)
+	r.DELETE("/books/:id", handler.deleteBookHandler)
+
+	r.Run()
+}
+
+type Handler struct {
+	db *gorm.DB
+}
+
+func newHandler(db *gorm.DB) *Handler {
+	return &Handler{db}
+}
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func (h *Handler) listBooksHandler(c *gin.Context) {
+	var books []Book
+
+	if result := h.db.Find(&books); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	}
+
+	c.JSON(http.StatusOK, &books)
+}
+
+func (h *Handler) createBookHandler(c *gin.Context) {
+	var book Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if result := h.db.Create(&book); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	}
+
+	c.JSON(http.StatusCreated, &book)
+}
+
+func (h *Handler) deleteBookHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	if result := h.db.Delete(&Book{}, id); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
